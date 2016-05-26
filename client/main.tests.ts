ad392fe955e7b46c2e8bb252be433b182b517d56
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-05-26 23:51:23 +0200

    init

---
+++ client/main.tests.ts
@@ -0,0 +1,8 @@
+import { mocha } from 'meteor/avital:mocha';
+import { chai, assert } from 'meteor/practicalmeteor:chai';
+
+describe('Test', () => {
+    it('should just run', () => {
+        assert.equal(5, 5);
+    });
+});
